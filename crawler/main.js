a3d34e324af58b4f9751a563d7966d90ffa154c3
dangzihei <1789896235@qq.com>
2022-11-05 18:17:23 +0800

    完善英雄列表爬虫

--- crawler/main.js
+++ crawler/main.js
@@ -1,20 +1,50 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const url = require('node:url');
+const baseUrl = require('./env.json')
 
-(async () => {
+
+let pptBrowser = undefined
+
+async function start() {
     // 启动浏览器
-    const browser = await puppeteer.launch({
+    pptBrowser = await puppeteer.launch({
         headless: false, // 默认是无头模式，这里为了示范所以使用正常模式
     })
 
+    let page = await pptBrowser.newPage()
+
+    await page.goto('https://101.qq.com/#/hero')
+
+    await page.waitForSelector('#app > div > div.app-main > div > div > ul > li:nth-child(1) > a')
+
+    let heroLinks = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll('.hero-list > li > a')).map(i => {
+            return i.href
+        })
+    })
+
+    for (let index = 0; index < heroLinks.length; index++) {
+        try {
+            await getHeroData(heroLinks[index])
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+   pptBrowser.close()
+}
+
+
+async function getHeroData(link) {
     // 控制浏览器打开新标签页面
-    const page = await browser.newPage()
+    let page = await pptBrowser.newPage()
     // 在新标签中打开要爬取的网页
-    await page.goto('https://101.qq.com/#/hero-detail?heroid=1&tab=overview&hero2_id=115&datatype=5v5')
+    await page.goto(link)
 
     // 使用evaluate方法在浏览器中执行传入函数（完全的浏览器环境，所以函数内可以直接使用window、document等所有对象和方法）
     await page.waitForSelector('#app > div > div.app-main > div > div.hero-details.hero-details-5v5 > div.hero-show > div.hero-intro > p')
-    
+
     let data = await page.evaluate(() => {
         let res = {}
         let heroName = document.querySelector('.hero-show .hero-intro .hero-name')
@@ -31,17 +61,23 @@ const fs = require('fs');
         res['dbRank'] = [];
         for (let i = 0; i < dbRank.length; i++) {
             res['dbRank'].push({
-                url:dbRank[i].getAttribute('src').slice(2),
-                alt:dbRank[i].getAttribute('alt')
+                url: dbRank[i].getAttribute('src').slice(2),
+                alt: dbRank[i].getAttribute('alt')
             })
         }
         return res
     })
+    // console.log(url.parse(link).hash.split('=')[1]);
+    // 此处是网站根目录../web/detail.data.json
+    // fs.writeFileSync(`../web/public/heros/detail.${url.parse(link).hash.split('=')[1]}.json`, JSON.stringify(data))
+    //发布时C:/inetpub/wwwroot/lol/detail.detail.${url.parse(link).hash.split('=')[1]}.json`.json
+    // fs.writeFileSync(`C:/inetpub/wwwroot/lol/heros/detail.${url.parse(link).hash.split('=')[1]}.json1`,JSON.stringify(data))
     
-    console.log(data)
-    // 此处是网站根目录../web/detail.data.json  
-    fs.writeFileSync('../web/public/detail.data.json',JSON.stringify(data))
-    //发布时C:/inetpub/wwwroot/lol/detail.data.json
-    // fs.writeFileSync('C:/inetpub/wwwroot/lol/detail.data.json',JSON.stringify(data))
-    browser.close()
-})()
+    fs.writeFileSync(baseUrl.basePath+`/detail.${url.parse(link).hash.split('=')[1]}.json`, JSON.stringify(data))
+    page.close()
+}
+
+start()
+console.log(123);
+
+setInterval(() => {}, 5000)
